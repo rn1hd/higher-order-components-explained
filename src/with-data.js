11e9e2ed6c602c4e1d37a52aba494ab6bfaa8c62
Spotlight64 <mamisayromnick@yahoo.com>
2022-04-01 16:18:11 +0800

    Initial commit from Create React App

---
+++ src/with-data.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const withData = (WrappedComponent) => {
+  class WithData extends React.Component {
+    state = {
+      data: [],
+    };
+
+    componentDidMount() {
+      fetch(this.props.dataSource)
+        .then((response) => response.json())
+        .then((data) => this.setState({ data: data.slice(0, 3) }));
+    }
+
+    render() {
+      const { dataSource, ...otherProps } = this.props;
+
+      return this.state.data.length < 1 ? (
+        <h1>LOADING</h1>
+      ) : (
+        <WrappedComponent data={this.state.data} {...otherProps} />
+      );
+    }
+  }
+
+  return WithData;
+};
+
+export default withData;
